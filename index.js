90a156578627e3d19b1bd0bad3236067f1789a44
Gigi-Lopah <gilbert2klopah@gmail.com>
2021-05-10 12:34:00 +0200

    Adding Features

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ const express = require('express');
 const app = express();
 const mysqlConnection = require('./databaseConnection');
 const upload = require('express-fileupload');
-
+const data = require("./testData")
 // Use functions
 app.use(express.static('public'));
 app.use(upload());    
 app.use(express.json({limit: '5mb'}));
 
-mysqlConnection.connect((err) => {
+/*mysqlConnection.connect((err) => {
     if(!err){
         console.log('connected');
     }else{
         throw err;
     }
-});
+});*/
 
-// Listen on port
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log('listening at  3000'));
+// Listen on port 
+const port = process.env.PORT || 4000
+app.listen(port, () => console.log('listening at  http://localhost:4000'));
