9bc158366fba3dcec5c19d6324ae536e8a267455
Mpumelelodube <dubempumelelo9@gmail.com>
2021-05-12 13:41:36 +0200

    update

--- index.js
+++ index.js
@@ -20,3 +20,32 @@ app.use(express.json({limit: '5mb'}));
 // Listen on port 
 const port = process.env.PORT || 4000
 app.listen(port, () => console.log('listening at  http://localhost:4000'));
+
+
+app.post('/register_tourist', (request, response) => {
+    // mysqlConnection.query('select * from products', (err, rows, fields) => {
+    //     if(!err){
+    //         //console.log(rows);
+    //         var data = rows; 
+    //         response.json(data);
+    //     }else{
+    //         throw err;
+    //     }
+    // })
+    console.log(request.body)
+    response.json(data);
+});
+
+app.post('/register_patner', (request, response) => {
+    // mysqlConnection.query('select * from products', (err, rows, fields) => {
+    //     if(!err){
+    //         //console.log(rows);
+    //         var data = rows; 
+    //         response.json(data);
+    //     }else{
+    //         throw err;
+    //     }
+    // })
+    console.log(request.body)
+    response.json(data);
+});
