3058db6f4511b12fb6195ff6dddbfeaedf5b97e7
diegoprospermurau <83754793+diegoprospermurau@users.noreply.github.com>
2021-05-17 14:39:56 +0200

    database update(17/05/21)

---
+++ afrotours data entry querries.sql
@@ -0,0 +1,25 @@
+ USE afrotour;
+-- INSERT INTO registered_tourists (forenames, last_name, date_of_birth, sex, email, contact_number, address, town, city, country)
+-- VALUES ("Glbert",
+-- 		"node.js",
+--         "2003/03/09",
+--         "male",
+--         "giribheti@gmail.com",
+--         "+263 715217831",
+--         "64 mbuya nehanda",
+--         "cbd",
+--         "harare",
+--         "zimbabwe"),
+--         ("jah mbume",
+-- 		"dube",
+--         "2002/11/07",
+--         "male",
+--         "rasta@gmail.com",
+--         "+263 715217551",
+--         "73 weed street",
+--         "amagedon",
+--         "smokecity",
+--         "jamaica")
+
+INSERT INTO tour_sites ()
+VALUES ()
